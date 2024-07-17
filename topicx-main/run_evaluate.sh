2167b6de92b518802d46ff815b261cfad5666e86
ahmadzeinabidmuhabbab <ahmadzeinabidmuhabbab@gmail.com>
2024-07-17 19:02:52 +0700

    first commit

---
+++ topicx-main/run_evaluate.sh
@@ -0,0 +1,14 @@
+DATASET='bbc'
+TOPIC_MODEL='cetopic'
+WORD_SELECT_METHOD='tfidf_idfi'
+PRETRAINED_MODEL='princeton-nlp/unsup-simcse-bert-base-uncased'
+
+
+python main.py\
+    --topic_model ${TOPIC_MODEL}\
+    --dataset ${DATASET}\
+    --num_topics 5\
+    --dim_size 5\
+    --word_select_method ${WORD_SELECT_METHOD}\
+    --pretrained_model ${PRETRAINED_MODEL}\
+    --seed 30
